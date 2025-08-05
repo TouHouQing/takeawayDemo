bc901073bd4bf1540ac8584480d985360e49f886
touhouqing <qinghaoyang@foxmail.com>
2025-08-05 18:08:54 +0800

    完成商品浏览功能代码

--- sky-take-out/sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-take-out/sky-server/src/main/java/com/sky/service/DishService.java
@@ -55,4 +55,11 @@ public interface DishService {
      * @param id
      */
     void startOrStop(Integer status, Long id);
+
+    /**
+     * 根据条件查询菜品和口味
+     * @param dish
+     * @return
+     */
+    List<DishVO>  listWithFlavor(Dish dish);
 }
